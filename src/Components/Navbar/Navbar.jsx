76e7ebf51e0313491df9ede8bd772e479a105e97
Sanket Verma <sanketv403@gmail.com>
2024-08-04 18:50:02 +0530

    update on 4th aug 2024

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className = "nav">
+        <div className="navbar">
+            <div className="logo"></div>
+
+            <div className="nav-links"></div>
+
+            <div className="button"><button></button></div>
+        </div>
+      
+    </div>
+  )
+}
