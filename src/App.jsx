09baf757d67ba781b487f13b77cb5ee25a603fc2
Sanket Verma <sanketv403@gmail.com>
2024-08-05 20:38:27 +0530

    update on 5th aug 2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-
-    </>
+    <Navbar/>
   )
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
-
+import img from '../images/img-1.png'
+import logo from "../images/Niki-Logo.jpg"
+import './Navbar.css'
 export default function Navbar() {
   return (
     <div className = "nav">
         <div className="navbar">
-            <div className="logo"></div>
+            <div className="logo"><img src={logo} alt="" /></div>
 
-            <div className="nav-links"></div>
+            <div className="nav-links">
+                <ul>
+                    <li className="link">
+                        Menu
+                    </li>
+                    <li className="link">
+                        Location
+                    </li>
+                    <li className="link">
+                        About
+                    </li>
+                    <li className="link">
+                        Contact
+                    </li>
 
-            <div className="button"><button></button></div>
+                </ul>
+            </div>
+
+            <div className="button"><button>Login</button></div>
         </div>
       
     </div>
